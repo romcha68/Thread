3fc33cb0d2e2224e38bf4bd66cf4d0dabb0b2b16
romcha68 <romcha68@ukr.net>
2018-02-15 12:55:03 +0200

    add TestThread1

---
+++ src/com/brainacad/oop/testthread1/Main.java
@@ -0,0 +1,11 @@
+package com.brainacad.oop.testthread1;
+
+/**
+ * Created by User on 15.02.2018.
+ */
+public class Main {
+    public static void main(String[] args) {
+        MyTimeBomb bomb1 = new MyTimeBomb();
+        bomb1.start();
+    }
+}
